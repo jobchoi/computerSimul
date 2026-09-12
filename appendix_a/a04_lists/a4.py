9c7d2c406d8749b27f7037741c83fc0b88f636b5
jobchoi <sedcreta12@gmail.com>
2026-09-12 16:36:05 +0900

    A.4.1

---
+++ appendix_a/a04_lists/a4.py
@@ -0,0 +1,19 @@
+a = []
+
+print(a)
+
+b = [1, 2, 3, "Arrival", False]
+
+print(f"b[0] => {b[0]}") 
+print(f"b[2] => {b[2]}")
+print(f"b[0:2] => {b[0:2]}")
+
+c = [0] * 6
+
+print(f"c => {c}")
+print(f"len(c) => {len(c)}")
+
+print(f"'Arrival' in b => {'Arrival' in b}")
+
+d = b+b
+print(f"d => {d}")
